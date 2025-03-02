0d549ee4923002e3bbe8bbf5f57092fafca9b955
wilson-rodriguez09 <samboniwilson09@gmail.com>
2025-03-02 14:54:54 -0500

    Ensayando commit modelo de usuario

--- back-end/apps/usuarios/usuario/models.py
+++ back-end/apps/usuarios/usuario/models.py
@@ -1,46 +1,36 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from apps.usuarios.rol.models import Rol
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from channels.layers import get_channel_layer
+from django.contrib.auth.hashers import make_password
 
 class UsuarioManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("El correo electrónico es obligatorio")
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+    def create_user(self, identificacion, password=None, **extra_fields):
+        if not identificacion:
+            raise ValueError("La identificación es obligatoria")
+
+        user = self.model(identificacion=identificacion, **extra_fields)
         user.set_password(password)  
-        user.save(using=self._db)
+        user.save(using=self._db)   
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, identificacion, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        if not extra_fields.get('is_staff'):
-            raise ValueError("El superusuario debe tener is_staff=True.")
-        if not extra_fields.get('is_superuser'):
-            raise ValueError("El superusuario debe tener is_superuser=True.")
-
-        return self.create_user(email, password, **extra_fields)
-
+        return self.create_user(identificacion, password, **extra_fields)
 
 class Usuarios(AbstractUser):
- 
+    username = None  # Eliminamos username porque usaremos identificación
+    identificacion = models.CharField(max_length=20, unique=True)  
     fk_id_rol = models.ForeignKey(Rol, on_delete=models.SET_NULL, null=True, blank=True)
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
-    email = models.EmailField(unique=True) 
-
-    USERNAME_FIELD = 'email'  
-    REQUIRED_FIELDS = ['username', 'nombre', 'apellido']  
+    email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = 'identificacion'  
+    REQUIRED_FIELDS = ['nombre', 'apellido', 'email']  
 
     objects = UsuarioManager()  
 
     def __str__(self):
-        return f"{self.nombre} {self.apellido}"
-
-
-
+        return f"{self.nombre} {self.apellido} - {self.identificacion}"
