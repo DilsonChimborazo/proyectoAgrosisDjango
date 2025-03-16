b2a9513c1a4035589c32a42d5b9e18943fc3725e
lucy2003fer <lucy48fernada@gmail.com>
2025-03-16 09:36:21 -0500

    mejoré un campo de venta, ahora automaticamente calcula el total de la venta

--- frontAgrosis/src/components/finanzas/venta/CrearVenta.tsx
+++ frontAgrosis/src/components/finanzas/venta/CrearVenta.tsx
@@ -1,4 +1,4 @@
-import { NuevaVenta} from '@/hooks/finanzas/venta/useCrearVenta';
+import { NuevaVenta } from '@/hooks/finanzas/venta/useCrearVenta';
 import { useCrearVenta } from '../../../hooks/finanzas/venta/useCrearVenta';
 import Formulario from '../../globales/Formulario';
 
@@ -9,16 +9,18 @@ const CrearVenta = () => {
         { id: 'fk_id_produccion', label: 'ID Producción', type: 'number' },
         { id: 'cantidad', label: 'Cantidad', type: 'number' },
         { id: 'precio_unidad', label: 'Precio por Unidad', type: 'number' },
-        { id: 'total_venta', label: 'Total de Venta', type: 'number' },
         { id: 'fecha', label: 'Fecha', type: 'date' },
     ];
 
     const handleSubmit = (formData: { [key: string]: string }) => {
+        const cantidad = parseFloat(formData.cantidad);
+        const precio_unidad = parseFloat(formData.precio_unidad);
+
         const nuevaVenta: NuevaVenta = {
             fk_id_produccion: formData.fk_id_produccion ? parseInt(formData.fk_id_produccion) : null,
-            cantidad: parseFloat(formData.cantidad),
-            precio_unidad: parseFloat(formData.precio_unidad),
-            total_venta: parseFloat(formData.total_venta),
+            cantidad,
+            precio_unidad,
+            total_venta: cantidad * precio_unidad, // Se calcula automáticamente
             fecha: formData.fecha,
         };
 
