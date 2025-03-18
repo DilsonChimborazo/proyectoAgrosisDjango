7cb7e4e33c060b355979f5ac44074e4c5ade77f8
lucy2003fer <lucy48fernada@gmail.com>
2025-03-17 19:51:50 -0500

    al borrar genera de la bd tuve que corregir algunas cosas en el serializer y views de produccion, que fue donde agregue la fk de cultivo

--- back-end/apps/finanzas/produccion/api/serializers.py
+++ back-end/apps/finanzas/produccion/api/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from apps.finanzas.produccion.models import Produccion
+from apps.trazabilidad.cultivo.api.serializers import LeerCultivoSerializer
 
 class ProduccionSerializer(ModelSerializer):
+    fk_id = LeerCultivoSerializer()
+    class Meta:
+        model = Produccion
+        fields = '__all__'  
+
+class escribirProduccionSerializer(ModelSerializer):
     class Meta:
         model = Produccion
         fields = '__all__'  
