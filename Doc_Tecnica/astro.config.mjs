da4f8333d21065754e529f4ec529992785dfcbd5
DavidJuan2810 <juandavides120@gmail.com>
2025-03-26 23:05:56 -0500

    Documentacion de las tablas sensores, eras, lotes y ubicacion del modulo de IOT

--- Doc_Tecnica/astro.config.mjs
+++ Doc_Tecnica/astro.config.mjs
@@ -29,6 +29,50 @@ export default defineConfig({
                     ],
 				},
 				{
+					label: 'ubicacion',
+					items: [
+                        { label: 'Creacion de ubicacion', slug: 'ubicacion/crearubicacion' },
+						{ label: 'Obtener ubicacion', slug: 'ubicacion/obtenerubicacion'},
+						{ label: 'Actuallizar ubicacion', slug: 'ubicacion/actualizarubicacion'},
+						{ label: 'Obtener ubicacion por identificacion', slug: 'ubicacion/obtenerubicacionbyid'},
+						{ label: 'Actualizar ubicacion por campo especifico', slug: 'ubicacion/patchubicacion'},
+						{ label: 'Eliminar ubicacion', slug: 'ubicacion/deleteubicacion'},
+                    ],
+				},
+				{
+					label: 'lote',
+					items: [
+                        { label: 'Creacion de lote', slug: 'lote/crearlote' },
+						{ label: 'Obtener lote', slug: 'lote/obtenerlote'},
+						{ label: 'Actuallizar lote', slug: 'lote/actualizarlote'},
+						{ label: 'Obtener lote por identificacion', slug: 'lote/obtenerlotebyid'},
+						{ label: 'Actualizar lote por campo especifico', slug: 'lote/patchlote'},
+						{ label: 'Eliminar lote', slug: 'lote/deletelote'},
+                    ],
+				},
+				{
+					label: 'eras',
+					items: [
+                        { label: 'Creacion de eras', slug: 'eras/creareras' },
+						{ label: 'Obtener eras', slug: 'eras/obtenereras'},
+						{ label: 'Actuallizar eras', slug: 'eras/actualizareras'},
+						{ label: 'Obtener eras por identificacion', slug: 'eras/obtenererasbyid'},
+						{ label: 'Actualizar eras por campo especifico', slug: 'eras/patcheras'},
+						{ label: 'Eliminar eras', slug: 'eras/deleteeras'},
+                    ],
+				},
+				{
+					label: 'sensores',
+					items: [
+                        { label: 'Creacion de sensores', slug: 'sensores/crearsensores' },
+						{ label: 'Obtener sensores', slug: 'sensores/obtenersensores'},
+						{ label: 'Actuallizar sensores', slug: 'sensores/actualizarsensores'},
+						{ label: 'Obtener sensores por identificacion', slug: 'sensores/obtenersensoresbyid'},
+						{ label: 'Actualizar sensores por campo especifico', slug: 'sensores/patchsensores'},
+						{ label: 'Eliminar sensores', slug: 'sensores/deletesensores'},
+                    ],
+				},
+				{
 					label: 'Reference',
 					autogenerate: { directory: 'reference' },
 				},
