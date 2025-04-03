dfef05ef95f3246c7f07a71ee646fb6b736bc2b1
lucy2003fer <lucy48fernada@gmail.com>
2025-04-03 09:58:18 -0500

    consulta para saber el costo total de insumos

--- back-end/apps/inventario/insumo/api/routers.py
+++ back-end/apps/inventario/insumo/api/routers.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from apps.inventario.insumo.api.views import insumoViewset
+from apps.inventario.insumo.api.views import InsumoViewSet
 
 router_insumo =DefaultRouter()
-router_insumo.register(prefix="insumo", basename= "insumo", viewset= insumoViewset) 
+router_insumo.register(prefix="insumo", basename= "insumo", viewset= InsumoViewSet) 
