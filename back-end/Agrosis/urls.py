7a2d573de028406234aa7822213850afd1868b31
lucy2003fer <lucy48fernada@gmail.com>
2025-04-03 18:39:20 -0500

    webhook para la consulta de egresos por insumos en actividades realizadas

--- back-end/Agrosis/urls.py
+++ back-end/Agrosis/urls.py
@@ -50,6 +50,7 @@ from apps.trazabilidad.tipo_cultivo.api.router import routerTipo_cultivo
 #rutas de finanzas
 from apps.finanzas.produccion.api.router import router_produccion
 from apps.finanzas.venta.api.router import router_venta
+from apps.inventario.insumo.api.webhooks import webhook_reporte_egresos
 
 #rutas de inventario
 from apps.inventario.control_usa_insumo.api.routers import router_usa
@@ -99,6 +100,7 @@ urlpatterns = [
     #FINANZAS
     path('api/', include(router_produccion.urls)),
     path('api/', include(router_venta.urls)),
+    path('api/webhook-reporte-egresos/', webhook_reporte_egresos, name='webhook-reporte-egresos'),
 
     #TRAZABILIDAD
     path('api/', include(router_actividad.urls)),
