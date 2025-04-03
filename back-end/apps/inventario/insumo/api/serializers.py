ea5e288c2188cf2474fc1c803e9aaec4f21053c2
lucy2003fer <lucy48fernada@gmail.com>
2025-04-03 09:58:18 -0500

    consulta para saber el costo total de insumos

--- back-end/apps/inventario/insumo/api/serializers.py
+++ back-end/apps/inventario/insumo/api/serializers.py
@@ -1,8 +1,57 @@
 from rest_framework.serializers import ModelSerializer
 from apps.inventario.insumo.models import Insumo
-
+from apps.trazabilidad.actividad.models import Actividad
+from apps.inventario.utiliza.models import Utiliza
+from rest_framework import serializers
 
 class InsumoSerializer(ModelSerializer):
     class Meta:
         model = Insumo
-        fields = '__all__'  
+        fields = '__all__'
+
+class ReporteEgresosSerializer(ModelSerializer):
+    actividad = serializers.CharField(source='nombre_actividad')
+    insumos = serializers.SerializerMethodField()
+    costo_total = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Actividad
+        fields = ['actividad', 'insumos', 'costo_total']
+
+    def get_insumos(self, obj):
+        # Obtener todas las utilizaciones de insumos para esta actividad
+        utilizaciones = []
+        for asignacion in obj.asignacion_actividades_set.all():
+            utilizaciones.extend(asignacion.utiliza_set.all())
+        
+        insumos_data = []
+        for utilizacion in utilizaciones:
+            insumo = utilizacion.fk_id_insumo
+            insumos_data.append({
+                'nombre': insumo.nombre,
+                'cantidad': utilizacion.cantidad,
+                'unidad_medida': insumo.unidad_medida,
+                'precio_unitario': float(insumo.precio_unidad)
+            })
+        
+        # Formatear como en el ejemplo
+        insumos_lista = ", ".join(
+            [f"{i['nombre']} {i['cantidad']} {i['unidad_medida']}" 
+             for i in insumos_data]
+        )
+        costos_lista = "+".join(
+            [str(i['precio_unitario']) for i in insumos_data]
+        )
+        
+        return {
+            'lista_insumos': insumos_lista if insumos_lista else "-",
+            'lista_costos': costos_lista if costos_lista else "-"
+        }
+
+    def get_costo_total(self, obj):
+        # Calcular el costo total para esta actividad
+        total = 0
+        for asignacion in obj.asignacion_actividades_set.all():
+            for utilizacion in asignacion.utiliza_set.all():
+                total += utilizacion.cantidad * float(utilizacion.fk_id_insumo.precio_unidad)
+        return total
--- back-end/apps/inventario/insumo/api/views.py
+++ back-end/apps/inventario/insumo/api/views.py
@@ -1,9 +1,47 @@
-from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Sum, F
 from apps.inventario.insumo.models import Insumo
-from apps.inventario.insumo.api.serializers import InsumoSerializer
+from apps.inventario.insumo.api.serializers import InsumoSerializer, ReporteEgresosSerializer
+from apps.trazabilidad.actividad.models import Actividad
+from apps.inventario.utiliza.models import Utiliza
+from django.db.models import Prefetch
+from apps.trazabilidad.asignacion_actividades.models import Asignacion_actividades
 
-class insumoViewset (ModelViewSet):
-    queryset = Insumo.objects.all()
+class InsumoViewSet(ModelViewSet):
     permissions_clases = [IsAuthenticatedOrReadOnly]
-    serializer_class = InsumoSerializer 
+    queryset = Insumo.objects.all()
+    serializer_class = InsumoSerializer
+
+    @action(detail=False, methods=['get'], url_path='reporte-egresos')
+    def reporte_egresos(self, request):
+        """
+        Endpoint para generar reporte de egresos por insumos agrupados por actividad
+        """
+        # Prefetch optimizado para las relaciones
+        actividades = Actividad.objects.prefetch_related(
+            Prefetch(
+                'asignacion_actividades_set',
+                queryset=Asignacion_actividades.objects.prefetch_related(
+                    Prefetch(
+                        'utiliza_set',
+                        queryset=Utiliza.objects.select_related('fk_id_insumo')
+                    )
+                )
+            )
+        ).all()
+
+        serializer = ReporteEgresosSerializer(actividades, many=True)
+        
+        # Calcular total general
+        total_general = sum(
+            item['costo_total'] for item in serializer.data
+        )
+        
+        return Response({
+            'reporte': serializer.data,
+            'total_general': total_general,
+            'estructura': "ACTIVIDADES | EGRESOS POR INSUMOS | TOTAL"
+        })
