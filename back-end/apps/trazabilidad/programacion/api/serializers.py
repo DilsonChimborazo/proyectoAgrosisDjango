7d9355e69ab85d442d7987adcf2e83b2bf893a02
lucy2003fer <lucy48fernada@gmail.com>
2025-04-03 09:15:56 -0500

    creación de una consulta para calcular el tiempo gastado en una actividad

--- back-end/apps/trazabilidad/programacion/api/serializers.py
+++ back-end/apps/trazabilidad/programacion/api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework.serializers import ModelSerializer
 from apps.trazabilidad.programacion.models import Programacion
 from apps.trazabilidad.asignacion_actividades.api.serializers import LeerAsignacion_actividadesSerializer
 from apps.trazabilidad.calendario_lunar.api.serializers import LeerCalendario_lunarSerializer
+from rest_framework import serializers
+from django.utils.dateparse import parse_duration
 
 class LeerProgramacionSerializer(ModelSerializer):
     fk_id_asignacionActividades = LeerAsignacion_actividadesSerializer()
@@ -15,3 +17,13 @@ class EscribirProgramacionSerializer(ModelSerializer):
     class Meta:
         model = Programacion
         fields = '__all__'
+
+
+class ReporteTiemposSerializer(serializers.Serializer):
+    actividad = serializers.CharField(source='fk_id_asignacionActividades__fk_id_actividad__nombre_actividad')
+    tiempo_total_minutos = serializers.SerializerMethodField()
+    veces_realizada = serializers.IntegerField()
+    
+    def get_tiempo_total_minutos(self, obj):
+        # Convertir DurationField a minutos
+        return obj['tiempo_total'].total_seconds() / 60
--- back-end/apps/trazabilidad/programacion/api/views.py
+++ back-end/apps/trazabilidad/programacion/api/views.py
@@ -1,13 +1,41 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from apps.trazabilidad.programacion.models import Programacion
-from apps.trazabilidad.programacion.api.serializers import LeerProgramacionSerializer, EscribirProgramacionSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Sum, Count
+from ..models import Programacion
+from .serializers import LeerProgramacionSerializer, EscribirProgramacionSerializer, ReporteTiemposSerializer
 
 class ProgramacionModelViewSet(ModelViewSet):
-    permissions_clases = [IsAuthenticatedOrReadOnly]
     queryset = Programacion.objects.all()
-
+    
     def get_serializer_class(self):
         if self.action in ['list', 'retrieve']:
             return LeerProgramacionSerializer
         return EscribirProgramacionSerializer
+
+    @action(detail=False, methods=['get'], url_path='reporte-tiempos')
+    def reporte_tiempos(self, request):
+        """
+        Endpoint para generar reporte de tiempos por actividad similar a la imagen proporcionada
+        """
+        # Agrupar por actividad y sumar tiempos
+        resultados = Programacion.objects.values(
+            'fk_id_asignacionActividades__fk_id_actividad__nombre_actividad'
+        ).annotate(
+            tiempo_total=Sum('duracion'),
+            veces_realizada=Count('id')
+        ).order_by('-tiempo_total')
+        
+        # Calcular total general
+        total_general = sum(item['tiempo_total'].total_seconds() / 60 for item in resultados)
+        
+        serializer = ReporteTiemposSerializer(resultados, many=True)
+        
+        return Response({
+            'actividades': serializer.data,
+            'total_general_minutos': total_general,
+            'estructura_reporte': [
+                "ACTIVIDAD | Tiempo dedicado (minutos) | TOTAL",
+                "--------------------------------------------"
+            ]
+        })
