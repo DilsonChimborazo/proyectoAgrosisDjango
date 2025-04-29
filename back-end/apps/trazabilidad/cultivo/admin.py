71299aaa1c363d3c2b5055aa0befdcfbc417783f
xiomarasabi <xiomarasabi2003@gmail.com>
2025-04-28 21:31:55 -0500

    cambios a las tablas de cultivo y plantacion

--- back-end/apps/trazabilidad/cultivo/admin.py
+++ back-end/apps/trazabilidad/cultivo/admin.py
@@ -3,4 +3,4 @@ from apps.trazabilidad.cultivo.models import Cultivo
 
 @admin.register(Cultivo)
 class CultivoAdmin(admin.ModelAdmin):
-    list_display = ('nombre_cultivo', 'fecha_plantacion','descripcion','fk_id_especie','fk_id_semillero') 
+    list_display = ('nombre_cultivo', 'fecha_plantacion','descripcion','fk_id_especie') 
