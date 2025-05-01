11e5f1386b0d54d2b2ba9b08f29ee44caf1cd622
wilson-rodriguez09 <samboniwilson09@gmail.com>
2025-05-01 18:36:02 -0500

    componente y hook para inteceptor de verificacion de token una parte

---
+++ frontAgrosis/src/components/inteceptor/Verificacion.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { isTokenExpired } from "@/hooks/inteceptor/axiosInstance"; 
+
+const AuthChecker = ({ children }: { children: React.ReactNode }) => {
+  const navigate = useNavigate();
+  const [showAlert, setShowAlert] = useState(false);
+
+  useEffect(() => {
+    const checkToken = () => {
+      const token = localStorage.getItem("token");
+      if (token && isTokenExpired(token)) {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        setShowAlert(true); // Muestra la alerta
+        setTimeout(() => {
+          setShowAlert(false); // Oculta la alerta después de 3 segundos
+          navigate("/"); // Redirige al login
+        }, 3000);
+      }
+    };
+
+    checkToken(); // Verifica al cargar
+
+    const interval = setInterval(checkToken, 5 * 60 * 1000); 
+
+    return () => clearInterval(interval);
+  }, [navigate]);
+
+  return (
+    <>
+      {showAlert && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-red-500 text-white p-4 rounded-lg shadow-lg">
+            Inicio de sesión expirado, vuelva a iniciar sesión
+          </div>
+        </div>
+      )}
+      {children}
+    </>
+  );
+};
+
+export default AuthChecker;
