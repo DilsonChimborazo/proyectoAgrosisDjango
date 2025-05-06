25ac02dda72d37919c0abe46a64577cdb9434740
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2025-05-05 20:59:35 -0500

    ajustes en realiza

--- back-end/apps/trazabilidad/realiza/api/serializers.py
+++ back-end/apps/trazabilidad/realiza/api/serializers.py
@@ -4,7 +4,7 @@ from apps.trazabilidad.cultivo.api.serializers import LeerCultivoSerializer
 from apps.trazabilidad.actividad.api.serializers import LeerActividadSerializer
 
 class LeerRealizaSerializer(ModelSerializer):
-    fk_id_cultivo = LeerCultivoSerializer()
+    fk_id_plantacion = LeerCultivoSerializer()
     fk_id_actividad = LeerActividadSerializer()
 
     class Meta:
