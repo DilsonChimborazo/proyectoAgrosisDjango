47db353bf1368fe4744b9c9328a6834b3da7fbf9
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2025-05-19 13:23:36 -0500

    tipado de crear bodega

--- frontAgrosis/src/hooks/inventario/bodega/useCrearBodega.ts
+++ frontAgrosis/src/hooks/inventario/bodega/useCrearBodega.ts
@@ -21,6 +21,82 @@ interface MovimientoBodegaPayload {
   insumos?: MovimientoInsumo[];
 }
 
+export interface Herramientas {
+  id: number;
+  nombre_h: string;
+  cantidad_herramienta: number;
+  estado: 'Disponible' | 'Prestado' | 'En reparacion';
+}
+
+export interface UnidadMedida {
+  id: number;
+  nombre_medida: string;
+  unidad_base: 'g' | 'ml' | 'u';
+  factor_conversion: number;
+}
+
+export interface Insumo {
+  id: number;
+  nombre: string;
+  tipo: string;
+  precio_unidad: number;
+  cantidad_insumo: number;
+  cantidad_en_base: string | null;
+  fecha_vencimiento: string;
+  img: string | null;
+  fk_unidad_medida: UnidadMedida;
+  precio_por_base: number;
+}
+
+export interface Asignacion {
+  id: number;
+  estado: 'Pendiente' | 'Completada' | 'Cancelada' | 'Reprogramada';
+  fecha_programada: string;
+  observaciones: string;
+  fk_id_realiza: Realiza;
+  fk_identificacion: Usuario;
+}
+
+export interface Realiza {
+  id: number;
+  fk_id_cultivo: Cultivo;
+  fk_id_actividad: Actividad;
+}
+
+export interface Cultivo {
+  id: number;
+  nombre_cultivo: string;
+  descripcion: string;
+  fk_id_especie: Especie;
+}
+
+export interface Especie {
+  id: number;
+  nombre_cientifico: string;
+  nombre_comun: string;
+  descripcion: string;
+  fk_id_tipo_cultivo: TipoCultivo;
+}
+
+export interface TipoCultivo {
+  id: number;
+  nombre: string;
+  descripcion: string;
+}
+
+export interface Actividad {
+  id: number;
+  nombre_actividad: string;
+  descripcion: string;
+}
+
+export interface Usuario {
+  id: number;
+  nombre: string;
+  apellido: string;
+  email: string;
+}
+
 export const useCrearBodega = () => {
   const queryClient = useQueryClient();
 
