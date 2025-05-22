0c5468aa390a4d8b9ee7ffeca1b930504e12b4c6
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2025-05-22 09:38:47 -0500

    base de datos nueva

--- back-end/Agrosis/settings.py
+++ back-end/Agrosis/settings.py
@@ -161,7 +161,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pruebafinal',
+        'NAME': 'laultimaprueba',
         'USER': 'postgres',                
         'PASSWORD': 'adso2024',      
         'HOST': 'localhost',                  
