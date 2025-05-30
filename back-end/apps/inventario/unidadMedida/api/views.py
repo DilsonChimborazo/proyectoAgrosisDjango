891e80e54aec5ddf8636f36e58515250ccc7f275
DilsonChimborazo <dilsonchimborazoperez@gmail.com>
2025-05-30 08:44:04 -0500

    ajustes unidadmedida

--- back-end/apps/inventario/unidadMedida/api/views.py
+++ back-end/apps/inventario/unidadMedida/api/views.py
@@ -1,9 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 from apps.inventario.unidadMedida.models import UnidadMedida
 from apps.inventario.unidadMedida.api.serilizers import UnidadMedidaSerializer
 
 class UnidadMedidaViewSet(ModelViewSet):
     queryset = UnidadMedida.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
     serializer_class = UnidadMedidaSerializer
